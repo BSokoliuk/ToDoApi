19942330ccd402bf813b658f27cc8043423627de
Bobozia <sokoliukbohdan@gmail.com>
2025-04-23 16:49:34 +0200

    add IRepository and ITodoItemRepository interfaces for repository pattern

---
+++ Domain/Interfaces/ITodoItemRepository.cs
@@ -0,0 +1,9 @@
+using Domain.Entities;
+
+namespace Domain.Interfaces;
+
+public interface ITodoItemRepository : IRepository<TodoItem>
+{
+  // Get items with expiry date greater than now and less than the given dateTime
+  Task<IEnumerable<TodoItem>> GetIncomingItemsAsync(DateTime dateTime);
+}
