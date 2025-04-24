a56ffa28ca0fb96d0885538ea975d46895013e9c
Bobozia <sokoliukbohdan@gmail.com>
2025-04-24 17:44:46 +0200

    add: implement TodoItemRepository with CRUD operations

---
+++ Infrastructure/Repositories/TodoItemRepository.cs
@@ -0,0 +1,44 @@
+using Domain.Entities;
+using Domain.Interfaces;
+using Infrastructure.DbContexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories;
+
+public class TodoItemRepository(TodoItemDbContext context) : ITodoItemRepository
+{
+  private readonly TodoItemDbContext _context = context;
+
+  public async Task<TodoItem?> GetByIdAsync(int id)
+  {
+    return await _context.TodoItems.AsNoTracking().FirstOrDefaultAsync(ti => ti.Id == id);
+  }
+
+  public async Task<IEnumerable<TodoItem>> GetAllAsync()
+  {
+    return await _context.TodoItems.ToListAsync();
+  }
+
+  public async Task<IEnumerable<TodoItem>> GetIncomingItemsAsync(DateTime dateTime)
+  {
+    return await _context.TodoItems
+      .Where(ti => ti.ExpiryDateTime > DateTime.UtcNow && ti.ExpiryDateTime < dateTime)
+      .ToListAsync();
+  }
+
+  public async Task<TodoItem> AddAsync(TodoItem entity)
+  {
+    var result = await _context.TodoItems.AddAsync(entity);
+    return result.Entity;
+  }
+
+  public void Update(TodoItem model)
+  {
+    _context.TodoItems.Update(model);
+  }
+
+  public void Delete(TodoItem model)
+  {
+    _context.TodoItems.Remove(model);
+  }
+}
